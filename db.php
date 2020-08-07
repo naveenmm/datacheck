90b89b5bec3c38c585a3a16e34ccfc6f99364edf
Naveen Mm <naveen.mm@soti.net>
2020-08-07 16:12:57 +0530

    db.php change

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
-<?php $username='soti';
-        $password='3jN@KG3uu@hSKXS';
-        $db='candidate_list';
-        $conn=mysqli_connect('localhost',$username,$password,$db);
+<?php $username='';
+        $password='';
+        $db='';
+        $conn=mysqli_connect('',$username,$password,$db);
